75a9536065ebda14cdedda7b35417bbfa4a8a408
Nathan McGinness <nathanmcginness@gmail.com>
2013-01-25 17:18:50 +1100

    argh cant get accordion to work

---
+++ javascripts/main.js
@@ -0,0 +1,12 @@
+// Can also be used with $(document).ready()
+$('document').ready( function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    slideshow: false
+  });
+  // $( "#accordion" ).accordion({ 
+  //   heightStyle: "content",
+  //   active: false,
+  //   collapsible: true, 
+  // });  
+});
