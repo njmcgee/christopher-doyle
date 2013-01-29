8a88b03abde5d53d01458563d90ee9a2882ba310
Nathan McGinness <nathanmcginness@gmail.com>
2013-01-29 18:01:06 +1100

    removed css3 on flexslider

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,7 @@ $('document').ready( function() {
   $('.flexslider').flexslider({
     animation: "slide",
     slideshow: false,
+    useCSS: false,
     start: function() {
       $( "#accordion" ).accordion({ 
           heightStyle: "content",
