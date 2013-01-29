8c0ae3c27cc982e41ea3588ad33e6531642f5ca3
Nathan McGinness <nathanmcginness@gmail.com>
2013-01-29 11:12:18 +1100

    fixed js issues

--- _site/javascripts/main.js
+++ _site/javascripts/main.js
@@ -2,11 +2,14 @@
 $('document').ready( function() {
   $('.flexslider').flexslider({
     animation: "slide",
-    slideshow: false
-  });
-  // $( "#accordion" ).accordion({ 
-  //   heightStyle: "content",
-  //   active: false,
-  //   collapsible: true, 
-  // });  
+    slideshow: false,
+    start: function() {
+      $( "#accordion" ).accordion({ 
+          heightStyle: "content",
+          active: false,
+          collapsible: true
+        });  
+    }
+  })
+
 });
